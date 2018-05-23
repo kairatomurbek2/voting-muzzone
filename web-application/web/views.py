bef52a94c3455b3fcb904de5dd421ed63c14f7bb
ilgizkasymov <ilgizkasymov@gmail.com>
2018-05-23 19:14:49 +0600

    test

--- web-application/web/views.py
+++ web-application/web/views.py
@@ -2,6 +2,7 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.db.models import Count
 
 from web.decorators import get_poll, get_choice, check_vote_permisson
 from web.models import PollAnswer, Poll
@@ -21,7 +22,10 @@ def vote(request, **kwargs):
 
 def feed(request):
     poll = Poll.objects.filter(is_active=True).last()
-    return render(request, 'base.html', {'choices': poll.get_result()})
+    if poll:
+        return render(request, 'base.html', {'choices': poll.get_result()})
+    else:
+        return render(request, 'test.html')
 
 @csrf_exempt
 @require_http_methods(["POST"])
